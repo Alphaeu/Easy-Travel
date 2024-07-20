7e42a214f5ebf51e9e28f43be22442f259931b93
Alphaeu <dealphaeus@gmail.com>
2024-07-20 15:27:23 +0300

    Backend revamp

--- easy-travel/routes/flights.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchFlights } = require('../controllers/flightController');
-
-// Search flights
-router.post('/search', searchFlights);
-
-module.exports = router;
--- easy-travel/routes/status.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFlightStatus } = require('../controllers/statusController');
-
-// Get flight status
-router.post('/', getFlightStatus);
-
-module.exports = router;
